5fefacd64fb70f3c9163d1840b72479db26db9cb
ishumsk <ivan.shumski@ibm.com>
2022-03-27 12:25:18 +0300

    create store, create register action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../../store/actions/register.action';
 
 @Component({
   selector: 'mc-register',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   public form: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -26,5 +28,12 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     console.log(this.form.value);
+    this.store.dispatch(
+      registerAction({
+        username: this.form.value.username,
+        email: this.form.value.email,
+        password: this.form.value.password,
+      })
+    );
   }
 }
