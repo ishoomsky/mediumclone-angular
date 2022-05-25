efc186abf03672764ad0d171a80b3f59d3a0bd2e
ishumsk <ivan.shumski@ibm.com>
2022-05-25 09:41:08 +0300

    implement effects, selector

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
 import { RegisterComponent } from 'src/app/auth/components/register/register.component';
 import { AuthService } from './services/auth.service';
+import { RegisterEffect } from './store/effects/register.effect';
 import { reducers } from './store/reducers';
 
 const routes: Routes = [
@@ -20,6 +22,7 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     ReactiveFormsModule,
     StoreModule.forFeature('auth', reducers),
+    EffectsModule.forFeature([RegisterEffect]),
   ],
   declarations: [RegisterComponent],
   providers: [AuthService],
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { AuthService } from '../../services/auth.service';
 import { registerAction } from '../../store/actions/register.action';
+import { isSubmittingSelector } from '../../store/selectors';
 
 @Component({
   selector: 'mc-register',
@@ -10,30 +14,44 @@ import { registerAction } from '../../store/actions/register.action';
 })
 export class RegisterComponent implements OnInit {
   public form: FormGroup;
+  public isSubmitting$: Observable<boolean>;
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
   }
 
   initializeForm(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
-      email: '',
-      password: '',
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     });
     console.log(this.form.valid);
   }
 
   onSubmit(): void {
-    console.log(this.form.value);
-    this.store.dispatch(
-      registerAction({
-        username: this.form.value.username,
-        email: this.form.value.email,
-        password: this.form.value.password,
-      })
-    );
+    // this.store.dispatch(
+    //   registerAction(this.form.value)
+    // );
+
+    const userData = {
+      user: this.form.value,
+    };
+    this.authService
+      .register(userData)
+      .subscribe((currentUser: CurrentUserInterface) =>
+        console.log(currentUser)
+      );
   }
 }
